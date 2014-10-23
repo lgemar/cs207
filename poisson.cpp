3487ecee88875bfd71f51693ad52615692c7c258
Lukas Gemar <lukasgemar@gmail.com>
2014-10-23 00:10:33 -0400

    I think that my remove_box was not working properly and now it is

--- poisson.cpp
+++ poisson.cpp
@@ -18,6 +18,7 @@
 #include "Point.hpp"
 #include "BoundingBox.hpp"
 #include <fstream>
+#include <list>
 
 
 /** Useful type information */
@@ -39,6 +40,14 @@ typedef typename GraphType::edge_type Edge;
 class GraphSymmetricMatrix {
 
 	/** TODO: 
+	 * [ ] Fix node iterator problem that causes invalidation of the last
+	 *  : node in graph upon node removal
+	 *	: when a node gets removed it becomes the end node
+	 *	: bug screws up viewer when there are no nodes in the graph
+	 * [ ] test whether adjacency iteration is working properly by trying to
+	 * 	   remove adjacent nodes. This will prove that the correct nodes
+	 * 	   are having their boundary value set to true
+	 *	: test did not work. Some adjacencies are removed, others not.
 	 * [x] flag whether a node is on a boundary
 	 *	: maybe I can do this within "remove_box" ? 
 	 *	: iterate along the adjacency list of the removed node and 
@@ -47,10 +56,9 @@ class GraphSymmetricMatrix {
 	 *	: graph.has_edge(Node i, Node j);  
 	 * [x] determine the degree of a node 
 	 *	: n.degree();
-	 * [ ] test whether adjacency iteration is working properly by trying to
-	 * 	   remove adjacent nodes. This will prove that the correct nodes
-	 * 	   are having their boundary value set to true
-	 * 
+	 * [x] check viewer to see if remove_box is working with "remove_list" 
+	 *		implementation as a workaround for the way "remove_node" 
+	 *		invalidates iterators
 	 */
 };
 
@@ -61,21 +69,29 @@ class GraphSymmetricMatrix {
  * @post all nodes adjacent to the removed edges are flagged as boundary nodes
  */
 void remove_box(GraphType& g, const BoundingBox& bb) {
+  std::list<Node> remove_list;
+
   for (auto it = g.node_begin(); it != g.node_end(); ++it) {
 	Node n = (*it);
 	Point p = n.position();
   	if( bb.contains(p) ) {
 		// Iterate through all the adjacent nodes and flag adjacent nodes
-		// as boundary nodes
+		// as boundary nodes; need to figure out if node is one edge length
+		// away from the boundary box
 		for (auto adj_it = n.edge_begin(); adj_it != n.edge_end(); ++adj_it) {
 			Edge e = *(adj_it);
 			Node adj_node = e.node2();
 			adj_node.value().boundary = true;
 		}
 		// Remove the node contained within the bounding box
-		g.remove_node(it);		
+		remove_list.push_front(n);
 	}
   }
+
+  /** Remove nodes in the remove list */
+  for (auto it = remove_list.begin(); it != remove_list.end(); ++it)
+		g.remove_node((*it));
+
   return;
 }
 
@@ -109,6 +125,12 @@ int main(int argc, char** argv)
     graph.add_edge(node_vec[t[0]], node_vec[t[2]]);
     graph.add_edge(node_vec[t[1]], node_vec[t[3]]);
     graph.add_edge(node_vec[t[2]], node_vec[t[3]]);
+  }
+  
+  /** Initialize the boundary values of the nodes */
+  for (auto it = graph.node_begin(); it != graph.node_end(); ++it) {
+	Node n = (*it);
+  	n.value().boundary = false;
   }
 
   // Get the edge length, should be the same for each edge
