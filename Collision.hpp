7c8dd25f0603dc417abc271ff35ba730d3332d58
Lukas Gemar <lukasgemar@gmail.com>
2014-12-03 11:00:26 -0500

    Added point printing functionality!

--- Collision.hpp
+++ Collision.hpp
@@ -1,12 +1,21 @@
 #include<iostream>
+#include "Point.hpp"
 
 typedef struct Collision {
 	/** Constructor */
 	Collision () {
 	}
 
+	/** Prints out a point to the console */
+	void print_point(Point p) {
+		std::cout << "(" << p.x << ", " << p.y << ", " << p.z << ")";
+	}
+
 	/** Runs a sequence of tests on the collision detector */
 	void test() {
 		std::cout << "This is a test" << std::endl;
+		Point p = Point(1, 2, 3);
+		std::cout<< "Here is a point: "; print_point(p);
+		std::cout << std::endl;
 	}
 } Collision;
